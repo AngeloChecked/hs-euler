75399b6e97cab60daf191bc7f75162e09c3482ea
Angelo Ceccato <angelo.ceccato@it.clara.net>
2020-09-19 18:04:04 +0200

    fix euler 4 and add some paolino solutions

--- src/Problem4.hs
+++ src/Problem4.hs
@@ -1,14 +1,24 @@
+{-# LANGUAGE TupleSections, ViewPatterns #-}
 module Problem4 where
+
+import Data.Function
+import Data.List 
+import Data.Ord
+import qualified Data.Map as MAP 
+import Control.Arrow 
 
 solution :: Int -> (Int, Int, Int) 
 solution digit = 
     let startNumber = maxNumberDigit digit in
-        f startNumber startNumber 
-            where f multiplying multiplied = 
+        maximum (f startNumber startNumber) 
+            where 
+                minNumDigit = (maxNumberDigit (digit-1))+1
+                f multiplying multiplied = 
                         let 
                             g x y 
-                             | isPalindrome (digs currentNum) = (currentNum, x, y) 
-                             | x > 1 = g (x-1) y 
+                             | y <= minNumDigit = []
+                             | isPalindrome (digs currentNum) = (currentNum, x, y) : g (x-1) y 
+                             | x > minNumDigit = g (x-1) y 
                              | otherwise = f (multiplying) (y-1) 
                              where currentNum = x*y 
                         in g multiplying multiplied
@@ -22,8 +32,75 @@ digs number = f number
 isPalindrome :: [Int] -> Bool
 isPalindrome numberSeq = numberSeq == (reverse numberSeq)
 
+isPalindrome2 n = reverseN2 n == n 
+
+reverseN :: Int -> Int 
+reverseN b = 
+    let digitNumber = floor (logBase 10 (fromIntegral b))+1 in
+    f b (digitNumber-1)
+        where f n 0 = n 
+              f n digit = 
+                  (n `mod` 10) * 10^digit + f (n `div` 10) (digit-1) 
+
+reverseN2 :: Integral b => b -> b
+reverseN2 x = snd $ fix $ \(fst -> q) -> r x $ 10 ^ q
+  where
+    r 0 n = (-1, 0)
+    r x n =
+      let (y, z) = divMod x 10
+       in succ *** (z * n +) $ r y (div n 10)
+
 maxNumberDigit :: Int -> Int 
 maxNumberDigit digit = (10^digit)-1  
 
 
+solution2 = maximum [(i*j,i,j) | i <- [x | x <- reverse [100..999], mod x 11 ==0], j <- reverse [100..999], i <= j, isPalindrome $ i * j]
+  where isPalindrome n = (read . reverse . show) n == n
+
+
+selectf :: (a -> Maybe (b, a)) -> [a] -> [(b, [a])]
+selectf h list = f list id 
+    where f [] _ = []
+          f (x:xs) g = case (h x) of
+                         (Just (b,a)) -> (g . (a:) <$> (b, xs)) : f xs (g . (x:))    
+                         Nothing -> f xs (g . (x:))    
+
+step :: (a -> a -> Ordering) -> [[a]] -> Maybe (a, [[a]])
+step h ls = case (selectf uncons ls) of
+                   [] -> Nothing
+                   ls -> Just $ maximumBy (h `on` fst) ls  
+
+prods m = inside <$> down n
+  where
+    n = pred $ z * 10
+    z = 10 ^ pred m
+    inside r = (r,) <$> down r
+    down x = [x, x - 1 .. z]
+
+driveStep :: (a -> a -> Ordering) -> [[a]] -> [a]
+driveStep g = unfoldr (step g)   
+
+enumerate n = driveStep (comparing $ uncurry (*)) (prods n)
+
+paolinoSolution = 
+    filter (\(_,_,n) -> reverseN n == n) $ take 6000 $ (\(x,y) -> (x, y , x * y)) <$> enumerate 3
+
+
+type Product = (Int, Int)
+type Queue = MAP.Map Int [Product]
+ 
+selectMap :: [(Int,Int)] -> Queue 
+selectMap chunk = MAP.fromList (f chunk id)
+        where f [] _ = []
+              f ((a,b):xs) g = (((a,b):) . g <$> (a*b,xs)) : f xs (g . ((a,b):)) 
+
+stepMap :: Queue -> [(Int, Product)]
+stepMap m = case MAP.lookupMax m of
+  Nothing -> []
+  Just (x, yh : yt) ->
+    let p = if isPalindrome2 x then ((x, yh) :) else id
+        f = case yt of
+          (a, b) : _ -> MAP.insert (a * b) yt
+          [] -> id
+     in p $ stepMap (f . MAP.delete x $ m)
 
--- test/Problem4Spec.hs
+++ test/Problem4Spec.hs
@@ -2,6 +2,8 @@ module Problem4Spec where
 
 import Test.Hspec
 import Problem4
+import Data.Ord
+import qualified Data.Map as MAP
 
 spec :: Spec
 spec = 
@@ -19,7 +21,33 @@ spec =
             maxNumberDigit 9 `shouldBe` 999999999 
         describe "largest palindome number made from the product of number digit" $ do
             it "with 2 digit" $ 
-               solution 2 `shouldBe` (9009, 91, 99) 
-            it "with 3 digit" $
-               solution 3 `shouldBe`  (90909, 91, 999) 
+               solution 2 `shouldBe` (9009, 99, 91) 
+            it "with 3 digit" $ do
+               solution 3 `shouldBe` (906609,993,913)
+            it "solution2: with 3 digit" $ do
+               solution2 `shouldBe`  (906609,913,993)
+        describe "paolino" $ do
+            it "digit of a number" $
+                floor (logBase 10 40005::Double)+1 `shouldBe` (5::Int)
+            it "reverse a number" $ do 
+                reverseN 3213231 `shouldBe` 1323123
+                reverseN2 3213231 `shouldBe` 1323123
+            it "for each number apply the functione in the list and move it to the head of tuple" $ do
+                selectf (\x -> if x `mod` 2 /= 0 then (Just (x,x^2)) else Nothing) [1..5] `shouldBe` [(1,[1,2,3,4,5]),(3,[1,2,9,4,5]),(5,[1,2,3,4,25])] 
+                selectf (\el -> if length el > 0 then (Just (head el, tail el)) else Nothing)  [([]::[Int]),[],[]] `shouldBe` [] 
+            it "head of the tuple is the bigest number combination" $ do 
+                step (comparing $ uncurry (*)) [[(99,97)],[(98,98),(98,97),(98,96)],[(97,97),(97,96),(97,95)]] `shouldBe` Just ((98,98),[[(99,97)],[(98,97),(98,96)],[(97,97),(97,96),(97,95)]]) 
+                step (comparing $ uncurry (*)) [[]] `shouldBe` Nothing 
+            it "return number combinations ordered by biggest number of all list heads" $
+                driveStep (comparing $ uncurry (*)) [[(99,97)],[(98,98),(98,97),(98,96)],[(97,97),(97,96),(97,95)]] `shouldBe` [(98,98),(99,97),(98,97),(97,97),(98,96),(97,96),(97,95)]
+            it "produce all progressive combinations, for each list the header is the biggest" $
+                (fmap (take 3) $ take 2 (prods 3)) `shouldBe` [[(999,999),(999,998),(999,997)],[(998,998),(998,997),(998,996)]]
+            it "get each progressive combination from bigger to smaller" $
+                (take 5 $ (\(x,y) -> (x, y , x * y)) <$> (enumerate 3))  `shouldBe` [(999,999,998001),(999,998,997002),(998,998,996004),(999,997,996003),(998,997,995006)] 
+            it "psolution" $
+                paolinoSolution `shouldBe` [(993,913,906609),(962,924,888888),(968,916,886688),(932,924,861168),(957,894,855558),(982,869,853358)]
+            
+
+
+             
 
